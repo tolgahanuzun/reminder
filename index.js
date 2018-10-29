88059cc96c8c0355b7bb9d70ac6ec6403af72f85
tolgahanuzun <mail@tolgahanuzun.com>
2018-10-29 13:13:18 +0300

    Insert and Get operations are defined.

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 var href_url = '';
-$("#reminders").ready(function(){
+document.body.onload = function() {
     url = document.getElementById('reminders-url');
     
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         try {
             url.innerHTML = tabs[0].url;
             href_url = tabs[0].url;
+            changeText(tabs[0].url)
         }
         catch(err) {
             console.log(err);
         }
     });
-});
 
-$("#save").click(function(){
-    console.log(href_url);
+}
 
-    var all_url = {}
-    chrome.storage.sync.get(['url'], function(result) {
-        all_url = result.keys;
+document.getElementById("save").onclick = function() {
+    text = document.getElementById('reminders-about').value;
+    var url = {};
+    url[href_url] = text;
+    chrome.storage.sync.set(url);
+    text = changeText(href_url)
+};
+
+function changeText(url) {
+    chrome.storage.sync.get([url], function(result) {
+        text = Object.entries(result)[0][1];
+        if (text) {
+            document.getElementById('reminder-text').innerHTML = text;
+        }
     });
-
-    text = document.getElementById('reminders-about');
-
-    save_url = {
-        url : href_url,
-        text : text
-    }
-
-    console.log(save_url)
-    console.log(all_url)
-    // Iki dizinin farkını almak gerekiyor. Ve storage setlenmesi gerekiyor.
-
-    chrome.storage.sync.set({ url : '1'}, function() {
-        console.log('Value is set to ' + '1');
-      });
-
-});
+}
